f460477e82e6552fbaccbbc65f61eb880985da6f
Angus Wong <158044675+wongy123@users.noreply.github.com>
2025-07-31 22:55:20 +1000

    Update InstallDocker.sh

--- InstallDocker.sh
+++ InstallDocker.sh
@@ -2,8 +2,8 @@
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get -y install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
+sudo apt-get install ca-certificates curl
+sudo install -m -y 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
