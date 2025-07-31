14a48f6ebef97d596f7fea2dc2ed39f2aa82cf2d
Angus Wong <158044675+wongy123@users.noreply.github.com>
2025-07-31 22:57:10 +1000

    Update InstallDocker.sh

--- InstallDocker.sh
+++ InstallDocker.sh
@@ -3,7 +3,7 @@
 # Add Docker's official GPG key:
 sudo apt-get update
 sudo apt-get install ca-certificates curl
-sudo install -m -y 0755 -d /etc/apt/keyrings
+sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
@@ -14,7 +14,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Check if the user is in the docker group
 if ! groups $USER | grep -q '\bdocker\b'; then
